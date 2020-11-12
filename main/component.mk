ff5e17a1c4f2921800f2d13d41a5f81e07c37012
h1romas4 <h1romas4@gmail.com>
2020-11-12 15:22:37 +0900

    add deley for watchdog timer

--- main/component.mk
+++ main/component.mk
@@ -2,3 +2,6 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+# for components/m5stack/M5Core2/src/Fonts/*.h
+CPPFLAGS += -Wno-missing-field-initializers
--- main/main.cpp
+++ main/main.cpp
@@ -8,4 +8,5 @@ void setup()
 void loop()
 {
     M5.update();
+    delay(1000);
 }
